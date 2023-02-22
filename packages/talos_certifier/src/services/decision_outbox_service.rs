aee204199f14b57af2f464ee6d4f267162e0faa6
Anil Sharma <asharmaanil@gmail.com>
2023-02-22 13:50:20 +1100

    fix: add agent header

--- packages/talos_certifier/src/services/decision_outbox_service.rs
+++ packages/talos_certifier/src/services/decision_outbox_service.rs
@@ -66,7 +66,7 @@ impl DecisionOutboxService {
             let decision_str = serde_json::to_string(&decision).unwrap();
             let mut decision_publish_header = HashMap::new();
             decision_publish_header.insert("messageType".to_string(), MessageVariant::Decision.to_string());
-
+            decision_publish_header.insert("certAgent".to_string(), decision.agent);
             debug!("Publishing message {}", decision.version);
             if let Err(publish_error) = publisher
                 .publish_message(xid.as_str(), &decision_str, Some(decision_publish_header.clone()))
