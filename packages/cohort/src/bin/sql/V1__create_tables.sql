d463a875b8570530e73d86862200cb583b324170
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2023-04-19 16:39:53 +1000

    feat: Add DB index to bank_accounts

--- packages/cohort/src/bin/sql/V1__create_tables.sql
+++ packages/cohort/src/bin/sql/V1__create_tables.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS bank_accounts (
     "data"      JSONB NOT NULL
 );
 
+CREATE INDEX bank_accounts_number ON bank_accounts((data->>'number'));
+
 CREATE TABLE IF NOT EXISTS cohort_snapshot (
     "id"        CHAR(9) PRIMARY KEY,       -- The value is always 'SINGLETON'
 	"version"   BIGINT NOT NULL
