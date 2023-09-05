610da8c01df452fa5fbd28272f33a62e5d904ec8
Marek Fedorovic <marek.fedorovic@kindredgroup.com>
2023-09-05 12:50:20 +1000

    fix: Disable coverage for cohort_sdk_js to see if this fixes build error.

--- scripts/coverage-report.sh
+++ scripts/coverage-report.sh
@@ -20,7 +20,7 @@ cargo +nightly build
 
 echo "Testing $app_name"
 export LLVM_PROFILE_FILE="${app_name}-%p-%m.profraw"
-cargo +nightly test --tests # don't run doctests
+cargo +nightly test --tests --workspace --exclude ${excludes} # don't run doctests
 
 rm ccov.zip 2> /dev/null || true
 zip -0 ccov.zip `find . \( -name "*.gc*" \) -print | grep -v ${excludes}`
@@ -38,7 +38,7 @@ grcov ccov.zip -s . --llvm  --ignore-not-existing --ignore "/*" --excl-start "\\
 rm ccov.zip
 
 # Re-run tests with JSON output
-cargo +nightly test --tests -- -Z unstable-options --format json --report-time > coverage/test-report.json
+cargo +nightly test --tests --workspace --exclude ${excludes} -- -Z unstable-options --format json --report-time > coverage/test-report.json
 
 if [ "$1" == "--open" ]; then
   index="file://$(pwd)/${base_dir}/../coverage/index.html"
