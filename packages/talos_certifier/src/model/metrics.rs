f5259cc572130b36dfc95323e37f25a37ed9486c
fmarek-kindred <123923685+fmarek-kindred@users.noreply.github.com>
2025-02-13 09:35:31 +1100

    Deserialise new metrics fields into default 0 (#118)
    * fix: Deserialise new metric fields with default values.
    
    * chore: Add deserialisation tests

--- packages/talos_certifier/src/model/metrics.rs
+++ packages/talos_certifier/src/model/metrics.rs
@@ -4,8 +4,10 @@ use serde::{Deserialize, Serialize};
 #[serde(rename_all = "camelCase")]
 pub struct TxProcessingTimeline {
     /// Cohort started certification
+    #[serde(default)]
     pub certification_started: i128,
     /// The request for certification is created
+    #[serde(default)]
     pub request_created: i128,
     /// Candidate published to kafka (agent time)
     pub candidate_published: i128,
@@ -17,4 +19,32 @@ pub struct TxProcessingTimeline {
     pub db_save_ended: i128,
     #[serde(skip_serializing, skip_deserializing)]
     pub decision_received_at: i128,
+}
+
+// $coverage:ignore-start
+#[cfg(test)]
+mod tests {
+    use crate::model::metrics::TxProcessingTimeline;
+
+    #[test]
+    fn deserialise_using_defaults() {
+        let json = r#"{
+            "candidatePublished": 1,
+            "candidateReceived": 2,
+            "candidateProcessingStarted": 3,
+            "decisionCreatedAt": 4,
+            "dbSaveStarted": 5,
+            "dbSaveEnded": 6
+        }"#;
+
+        let deserialised: TxProcessingTimeline = serde_json::from_str(json).unwrap();
+        assert_eq!(deserialised.certification_started, 0);
+        assert_eq!(deserialised.request_created, 0);
+        assert_eq!(deserialised.candidate_published, 1);
+        assert_eq!(deserialised.candidate_received, 2);
+        assert_eq!(deserialised.candidate_processing_started, 3);
+        assert_eq!(deserialised.decision_created_at, 4);
+        assert_eq!(deserialised.db_save_started, 5);
+        assert_eq!(deserialised.db_save_ended, 6);
+    }
 }
