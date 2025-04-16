284f094697b60f0dac6590af44f68c4dc22db2cb
Gethyl Kurian <gethyl.kurian@kindredgroup.com>
2025-04-16 14:37:18 +1000

    chore: certifier offset commit should maintain some diff based on config to repopulate the suffix on reset

--- packages/talos_certifier/src/services/mod.rs
+++ packages/talos_certifier/src/services/mod.rs
@@ -6,7 +6,7 @@ mod message_receiver_service;
 pub use certifier_service::{CertifierService, CertifierServiceConfig};
 pub use decision_outbox_service::DecisionOutboxService;
 pub use healthcheck_service::HealthCheckService;
-pub use message_receiver_service::MessageReceiverService;
+pub use message_receiver_service::{MessageReceiverService, MessageReceiverServiceConfig};
 
 #[cfg(test)]
 pub mod tests;
